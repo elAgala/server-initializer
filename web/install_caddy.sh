0a7e94b190e7b5d76a80bdcf201fc900f3439f14
elAgala <julian@benitez.com.ar>
2025-01-26 05:10:27 -0300

    Rename coraza_rules.conf -> coraza.conf

--- web/install_caddy.sh
+++ web/install_caddy.sh
@@ -17,8 +17,9 @@ function install_caddy() {
   chown -R "$username:$username" "$caddy_dir"
 
   wget "$REPO_URL/$TEMPLATE_PATH/docker-compose.yml" -O "$caddy_dir/docker-compose.yml"
+  wget "$REPO_URL/$TEMPLATE_PATH/.env" -O "$caddy_dir/.env"
   wget "$REPO_URL/$TEMPLATE_PATH/caddy/Caddyfile" -O "$caddy_dir/caddy/Caddyfile"
-  wget "$REPO_URL/$TEMPLATE_PATH/caddy/coraza/coraza_rules.conf" -O "$caddy_dir/caddy/coraza/coraza_rules.conf"
+  wget "$REPO_URL/$TEMPLATE_PATH/caddy/coraza/coraza.conf" -O "$caddy_dir/caddy/coraza/coraza.conf"
   wget "$REPO_URL/$TEMPLATE_PATH/crowdsec/acquis.yaml" -O "$caddy_dir/crowdsec/acquis.yaml"
 
   echo "[ WEB ]: Caddy setup succesfully. You can find the Caddyfile under /home/$username/web_server/caddy"
