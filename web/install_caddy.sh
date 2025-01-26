96f839bde1709b50a63a9d036b0983ec9f832183
elAgala <julian@benitez.com.ar>
2025-01-26 01:42:22 -0300

    Minor permission fixes

--- web/install_caddy.sh
+++ web/install_caddy.sh
@@ -3,7 +3,7 @@
 function install_caddy() {
 
   REPO_URL="https://raw.githubusercontent.com/elAgala/server-initializer/master"
-  TEMPLATE_PATH="/templates/caddy/full"
+  TEMPLATE_PATH="templates/caddy/full"
 
   username="$1"
   caddy_dir="/home/$username/web-server"
@@ -14,12 +14,14 @@ function install_caddy() {
   mkdir -p "$caddy_dir/caddy"
   mkdir -p "$caddy_dir/caddy/coraza"
 
+  chown -R "$username:$username" "$caddy_dir"
+
   wget "$REPO_URL/$TEMPLATE_PATH/docker-compose.yml" -O "$caddy_dir/docker-compose.yml"
   wget "$REPO_URL/$TEMPLATE_PATH/caddy/Caddyfile" -O "$caddy_dir/caddy/Caddyfile"
   wget "$REPO_URL/$TEMPLATE_PATH/caddy/coraza/coraza_rules.conf" -O "$caddy_dir/caddy/coraza/coraza_rules.conf"
   wget "$REPO_URL/$TEMPLATE_PATH/crowdsec/acquis.yaml" -O "$caddy_dir/crowdsec/acquis.yaml"
 
-  echo "[ WEB ]: Caddy setup succesfully. You can find the Caddyfile under /home/$username/caddy/settings"
+  echo "[ WEB ]: Caddy setup succesfully. You can find the Caddyfile under /home/$username/web_server/caddy"
   echo "[ WEB ]: Do not forget to update the .env file located under $caddy_dir"
   docker network create caddy_net
   echo "[ WEB ]: Created caddy intranet 'caddy_net'"
