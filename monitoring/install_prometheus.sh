32a09ed129ec2833e802d3542938cb4dc273b85e
elAgala <julian@benitez.com.ar>
2025-08-12 18:51:44 -0300

    save

--- monitoring/install_prometheus.sh
+++ monitoring/install_prometheus.sh
@@ -10,6 +10,7 @@ function install_prometheus() {
   mkdir -p "$monitoring_dir"
   mkdir -p "$monitoring_dir/loki"
   mkdir -p "$monitoring_dir/promtail"
+  mkdir -p "$monitoring_dir/alerts"
   
   # Download main monitoring files
   wget "$REPO_URL/$TEMPLATE_PATH/docker-compose.yml" -O "$monitoring_dir/docker-compose.yml"
@@ -20,6 +21,11 @@ function install_prometheus() {
   
   # Download Promtail configuration
   wget "$REPO_URL/$TEMPLATE_PATH/promtail/promtail.yml" -O "$monitoring_dir/promtail/promtail.yml"
+  
+  # Download alert rules
+  wget "$REPO_URL/$TEMPLATE_PATH/alerts/infrastructure.yml" -O "$monitoring_dir/alerts/infrastructure.yml"
+  wget "$REPO_URL/$TEMPLATE_PATH/alerts/web-server.yml" -O "$monitoring_dir/alerts/web-server.yml"
+  wget "$REPO_URL/$TEMPLATE_PATH/alerts/docker.yml" -O "$monitoring_dir/alerts/docker.yml"
   
   cd "$monitoring_dir"
   echo "[ MONITOR ]: Monitoring stack installed. Starting containers"
